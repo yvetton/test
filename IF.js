f92dfde063197bdb8f040789d941d2f2f0f61e6c
yvetton <isiyvetton@gmail.com>
2023-08-30 22:24:02 +0200

    ALL FOR STUDT STUFFS

---
+++ IF.js
@@ -0,0 +1,36 @@
+let year=2022;
+
+if (year==2023){
+console.log('UMWAKA MWIZA');
+}
+else if (year<2023){
+    console.log('MWASIZEMO UYUMWAKA  twarawurenze.');
+}
+else if (year>2023){
+    console.log('MWASIZEMO UYUMWAKA  NTITURAWUGERAMO.');
+}
+let marks=20;
+if (marks>=50)
+{
+    console.log("wemerewe gukomeza kwiga");
+}
+else if(marks<50)
+{
+    console.log("ntiwemerewe gukomeza kwiga");
+}
+
+let i=1;
+
+while (i<10)
+{
+console.log(i)
+i++;
+}
+
+
+let a;
+for(a=1;a<=20;a++){
+if (a==5)
+continue;
+console.log(a);
+}
